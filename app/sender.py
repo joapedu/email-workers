36b2e4cc461d12632ef239752bcfe202fd411214
joapedu <joaoeduardobraga2@gmail.com>
2023-10-19 16:17:17 -0300

    :sparkles: feat: add new workers to the proj

--- app/sender.py
+++ app/sender.py
@@ -1,29 +1,40 @@
 import psycopg2
-from bottle import route, run, request
+import redis
+import json
+from bottle import Bottle, request
 
-DSN = 'dbname=email_sender user=postgres host=db'
-SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
 
-def register_message(assunto, mensagem):
-    conn = psycopg2.connect(DSN)
-    cur = conn.cursor()
-    cur.execute(SQL, (assunto, mensagem))
-    conn.commit()
-    cur.close()
-    conn.close()
-    
-    print(' Mensagem registrada! ')
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
+        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
+        DSN = 'dbname=email_sender user=postgres host=db'
+        self.conn = psycopg2.connect(DSN)
 
-@route('/', method='POST')
-def send():
-    assunto = request.forms.get('assunto')
-    mensagem = request.forms.get('mensagem')
-    
-    register_message(assunto, mensagem)
-    
-    return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
-        assunto, mensagem
-    )
-    
+    def register_message(self, assunto, mensagem):
+        SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+        cur = self.conn.cursor()
+        cur.execute(SQL, (assunto, mensagem))
+        self.conn.commit()
+        cur.close()
+
+        msg = {'assunto': assunto, 'mensagem': mensagem}
+        self.fila.rpush('sender', json.dumps(msg))
+
+        print(' Mensagem registrada! ')
+
+    def send(self):
+        assunto = request.forms.get('assunto')
+        mensagem = request.forms.get('mensagem')
+
+        self.register_message(assunto, mensagem)
+
+        return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
+            assunto, mensagem
+        )
+
+
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    sender = Sender()
+    sender.run(host='0.0.0.0', port=8080, debug=True)
---
+++ worker/app.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pip install edis==2.10.5
+python -u worker.py
