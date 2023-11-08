f87b0cc3921a9901a8148c5e9ec93c1890ac63e9
edurs2602 <edurs.2602@gmail.com>
2023-11-08 12:00:02 -0300

    feat: Adicionando simples tratamento de erros ao código

--- app/sender.py
+++ app/sender.py
@@ -4,43 +4,55 @@ import json
 import os
 from bottle import Bottle, request
 
-
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
         
-        redis_host = os.getenv('REDIS_HOST', 'queue')
-        self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
-        
-        db_host = os.getenv('DB_HOST', 'db')
-        db_user = os.getenv('DB_USER', 'postgres')
-        db_name = os.getenv('DB_NAME', 'sender')
-        dsn = f'dbname={db_name} user={db_user} host={db_host}'
-        self.conn = psycopg2.connect(dsn)
+        try:
+            redis_host = os.getenv('REDIS_HOST', 'queue')
+            self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+            
+            db_host = os.getenv('DB_HOST', 'db')
+            db_user = os.getenv('DB_USER', 'postgres')
+            db_name = os.getenv('DB_NAME', 'sender')
+            dsn = f'dbname={db_name} user={db_user} host={db_host}'
+            self.conn = psycopg2.connect(dsn)
+
+        except Exception as e:
+            print(f'Erro ao inicializar: {e}')
+            # Você pode adicionar ações adicionais de tratamento de erro aqui
 
     def register_message(self, assunto, mensagem):
-        SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
-        cur = self.conn.cursor()
-        cur.execute(SQL, (assunto, mensagem))
-        self.conn.commit()
-        cur.close()
+        try:
+            SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+            cur = self.conn.cursor()
+            cur.execute(SQL, (assunto, mensagem))
+            self.conn.commit()
+            cur.close()
 
-        msg = {'assunto': assunto, 'mensagem': mensagem}
-        self.fila.rpush('sender', json.dumps(msg))
+            msg = {'assunto': assunto, 'mensagem': mensagem}
+            self.fila.rpush('sender', json.dumps(msg))
 
-        print(' Mensagem registrada! ')
+            print(' Mensagem registrada! ')
+        
+        except Exception as e:
+            print(f'Erro ao registrar mensagem: {e}')
+            # Adicione ações adicionais de tratamento de erro aqui
 
     def send(self):
-        assunto = request.forms.get('assunto')
-        mensagem = request.forms.get('mensagem')
+        try:
+            assunto = request.forms.get('assunto')
+            mensagem = request.forms.get('mensagem')
 
-        self.register_message(assunto, mensagem)
+            self.register_message(assunto, mensagem)
 
-        return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
-            assunto, mensagem
-        )
-
+            return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
+                assunto, mensagem
+            )
+        
+        except Exception as e:
+            return f'Erro ao processar a solicitação: {e}'
 
 if __name__ == '__main__':
     sender = Sender()
